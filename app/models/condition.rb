4d6f93a222358e06de29a92d39f31d552bf4f3eb
Mart-rock <itsmyrockandrollmart34@gmail.com>
2020-08-15 19:58:15 +0900

    修正

--- app/models/condition.rb
+++ app/models/condition.rb
@@ -1,5 +1,5 @@
 class Condition < ActiveHash::Base
   self.data = [
-    {id: 1, name: '新品、未使用'}, {id: 2, name: '目立った汚れ傷なし'}, {id: 3, name: '傷や汚れ有り'}, {id: 4, name: '全体的に状態が悪い'}
+    {id: 0, name: '選択して下さい'}, {id: 1, name: '新品、未使用'}, {id: 2, name: '目立った汚れ傷なし'}, {id: 3, name: '傷や汚れ有り'}, {id: 4, name: '全体的に状態が悪い'}
 ]
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -9,8 +9,11 @@ class Item < ApplicationRecord
 
   validates :price, presence: true, numericality: { greater_than_or_equal_to:50 ,less_than_or_equal_to:10000000 }
   validates :name, :text, :item_images, presence: true
-  validates :category_id, presence: true, inclusion: { in: [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47] }
-  validates :condition_id, :shipping_charges_id, :shipping_area_id, :days_until_delivery_id, presence: true
+  validates :shipping_area_id, presence: true, inclusion: { in: [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47] } 
+  validates :category_id, presence: true
+  validates :condition_id, presence: true, inclusion: { in: [1,2,3,4]}
+  validates :shipping_charges_id, presence: true, inclusion: { in: [1,2]}
+  validates :days_until_delivery_id, presence: true, inclusion: { in: [1,2,3]}
 
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to_active_hash :condition
